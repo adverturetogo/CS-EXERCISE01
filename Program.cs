844f9c7caae63f7e5a3df40b930b275af0713305
adverturetogo <2323100321@qq.com>
2025-09-09 13:07:27 +0800

    111

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Program.cs
+namespace LibrarySystem
+{
+    // 定义枚举
+    public enum ItemType { Novels, Magazine, TextBook }
+
+    // 定义基类
+    public abstract class LibraryItem
+    {
+        private readonly string _id;
+        private readonly string _title;
+        
+        public string Id => _id;
+        public string Title => _title;
+        public ItemType Type { get; set; }
+
+        public LibraryItem(string id, string title, ItemType type)
+        {
+            _id = id;
+            _title = title;
+            Type = type;
+        }
+
+        public abstract string GetDetails();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 测试代码...
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
